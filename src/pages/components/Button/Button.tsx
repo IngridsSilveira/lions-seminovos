54b7532eeb4093515f4f9e3b61e2adea0a6eed1b
Ingrid Souza <ingridsouzaok@gmail.com>
2024-10-03 14:07:56 -0300

    hover alterado

--- src/pages/components/Button/Button.tsx
+++ src/pages/components/Button/Button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps {
 export const Button: React.FC<ButtonProps> = ({ texto, width }) => {
   return (
     <a
-      className={`py-3 bg-vermelho text-white font-bold font-roboto hover:bg-yellow-600 hover:transition-all hover:delay-150 rounded-xl text-white m-auto text-center`}
+      className={`py-3 bg-vermelho text-white font-bold font-roboto hover:bg-red-600 hover:transition-all hover:delay-150 rounded-xl text-white m-auto text-center`}
       style={{ width }}
       href="https://www.lionsseminovos.com.br/"
       aria-label="Visite nosso site"
